7f2a0b4f339ce63a6c8c12f3c05ae4442a1ca96f
Alfie <alfye123@hotm.com>
2023-05-03 18:47:11 +0100

    added auth for client with input validation on api endpoint

--- apps/ml-site/src/pages/home/Home.tsx
+++ apps/ml-site/src/pages/home/Home.tsx
@@ -3,9 +3,15 @@ import { useEffect } from "react";
 function Home() {
   useEffect(() => {
     async function fetchData() {
-      const url = `${import.meta.env.VITE_API_URL}/checkfts`;
+      const url = `${import.meta.env.VITE_API_URL}/check_fts`;
 
       const response = await fetch(url);
+
+      if (response.status == 200) {
+        window.location.href = "/login";
+      } else {
+        window.location.href = "/register";
+      }
     }
 
     fetchData();
