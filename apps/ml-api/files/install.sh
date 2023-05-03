a37ef13b0bcd5439229b2851f66d85b50346bd46
Peter <peter.bedford2@gmail.com>
2023-05-03 19:35:51 +0100

    feat auto update for linux working

--- apps/ml-api/files/install.sh
+++ apps/ml-api/files/install.sh
@@ -7,6 +7,8 @@ INSTALL_DIR="/opt/MonitorLizardDaemon"
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 SERVICE_USER="monitorlizarddaemon"
 SERVICE_GROUP="monitorlizarddaemon"
+CRON_SCHEDULE="0 3 * * *"
+CRON_COMMAND="curl -sSL \"http://192.168.60.114:8001/install.sh\" | sudo bash -"
 
 
 # Check if Python is installed, and if not, download and install it
@@ -48,6 +50,18 @@ User=$SERVICE_USER
 [Install]
 WantedBy=multi-user.target
 EOF
+
+# Create a temporary file for the cron job
+CRON_TEMP_FILE=$(mktemp)
+
+# Write the cron job to the temporary file
+echo "$CRON_SCHEDULE $CRON_COMMAND" >> "$CRON_TEMP_FILE"
+
+# Add the cron job to the crontab for the root user
+sudo crontab "$CRON_TEMP_FILE"
+
+# Remove the temporary file
+rm "$CRON_TEMP_FILE"
 
 # Reload systemd to pick up new service file
 systemctl daemon-reload
