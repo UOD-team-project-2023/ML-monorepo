9310f8c7041f0d1a9f08391e1f521e5767fe634d
Peter <peter.bedford2@gmail.com>
2023-05-03 19:21:22 +0100

    feat-auto update working on Windows

--- apps/ml-api/files/install.ps1
+++ apps/ml-api/files/install.ps1
@@ -2,13 +2,17 @@
 $downloadUrl = "http://192.168.60.114:8001/file/client.zip"
 $pythonDownloadURL = "https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe"
 $pythonExe = "python.exe"
-$serviceName = "Monitor Lizard Probe2"
-$serviceDesc = "Monitor Lizard Probe2"
+$serviceName = "Monitor Lizard Probe"
+$serviceDesc = "Monitor Lizard Probe"
 $installDir = "C:\Program Files\Monitor Lizard Daemon"
 $nssmDir =  "C:\Program Files\Monitor Lizard Daemon\nssm"
 $logDir = "C:\Program Files\Monitor Lizard Daemon\logs"
 $service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
 $ErrorActionPreference = "Stop"
+# Define the task name, description, and command
+$taskName = "MonitorLizardUpdate"
+$taskDesc = "Update MonitorLizard every 24 hours at 3am"
+$taskCommand = "Start-Process powershell.exe -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass -Command (Invoke-WebRequest -Uri ''http://192.168.60.114:8001/install.ps1'' -UseBasicParsing | Invoke-Expression)'"
 
 # Check if Python is installed, and if not, download and install it
 if (-not (Test-Path $pythonExe)) {
@@ -43,6 +47,15 @@ if($service -eq $null)
     ./nssm.exe set $serviceName AppExit Default Exit
     ./nssm.exe set $serviceName AppStdout "$logDir\service.log"
 } 
+
+# Create the trigger for the scheduled task
+$trigger = New-ScheduledTaskTrigger -Daily -At 3am
+
+# Create the action for the scheduled task
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $taskCommand
+
+# Register the scheduled task with the Task Scheduler
+Register-ScheduledTask -TaskName $taskName -Description $taskDesc -Trigger $trigger -Action $action -RunLevel Highest
 
 # Start the service
 echo "Starting service."
